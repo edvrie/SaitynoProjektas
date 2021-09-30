0fe01304561fdb9c418c88877978b45d32de4d7e
Edvinas Riepsas <edvinas.riepsas@gmail.com>
2021-09-30 22:14:55 +0300

    Added URL validations, json format validations and fixed responses

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,3 +1,4 @@
+import e from 'express';
 import express from 'express';
 
 // IMPORTTANT: THIS MIGHT BREAK SINCE IT CAN NO LONGER FIND THE IMPORT
@@ -8,6 +9,17 @@ const port = 3000;
 // JSON parse
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+});
+
+app.use((err, req, res, next) => {
+    res.status(400).send({
+        status: 400,
+        error: err.message
+    });
+});
 
 const themes = [
     {
@@ -37,12 +49,20 @@ const posts = [
 
 const comments = [
     {
-        id: "4887a",
+        id: "4887",
         postId: "751213",
         userId: "48753asee",
         text: "This is true"
     }
 ];
+
+const parseIds = (id: string) => {
+    const idInt = Number(id);
+    console.log(idInt)
+    if (isNaN(idInt)) {
+        throw new Error("Invalid URL");
+    }
+};
 
 // DEFAULT 
 app.get('/', (req, res) => {
@@ -52,76 +72,101 @@ app.get('/', (req, res) => {
 // GET LISTED
 app.get('/api/themes', (req, res) => {
     try{
-        themes ? res.json(themes) : res.sendStatus(404);
+        themes ? res.json(themes) : res.status(404).send({
+            status: 404,
+            error: "Not found"
+        });
     } catch {
-        res.status(404).send('Get failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
 app.get('/api/themes/:id/posts', (req, res) => {
     try {
-        posts ? res.json(posts) : res.sendStatus(404);
+        parseIds(req.params.id);
+        posts ? res.json(posts) : res.status(404).send({
+            status: 404,
+            error: "Not found"
+        });
     } catch {
-        res.status(404).send('Get failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
-app.get('/api/themes/:id/posts/:id/comments', (req, res) => {
+app.get('/api/themes/:themeId/posts/:postId/comments', (req, res) => {
     try {
-        comments ? res.json(comments) : res.sendStatus(404);
+        parseIds(req.params.themeId);
+        parseIds(req.params.postId);
+        comments ? res.json(comments) : res.status(404).send({
+            status: 404,
+            error: "Not found"
+        });
     } catch {
-        res.status(404).send('Get failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
-
-// USING QUERIES
-
-// app.get('/posts/', (req, res) => {
-//     try {
-//         const query = req.query.id;
-//         res.status(200).json(posts);
-//     } catch {
-//         res.status(404).send('Get failed. Please try again.');
-//     };
-// });
-
-// app.get('/comments/', (req, res) => {
-//     try {
-//         const query = req.query.id;
-//         res.status(200).json(comments);
-//     } catch {
-//         res.status(404).send('Get failed. Please try again.');
-//     }
-// });
 
 // GET BY ID
 app.get('/api/themes/:id', (req, res) => {
     try {
+        parseIds(req.params.id);
         const id = req.params.id;
         const theme = themes.find((theme) => theme.id === parseInt(id));
-        theme ? res.send(theme) : res.sendStatus(404);
+        theme ? res.send(theme) : res.status(404).send({
+            status: 404,
+            error: "Not found"
+        });
     } catch {
-        res.status(400).send('Get failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
 app.get('/api/themes/:themeId/posts/:postId', (req, res) => {
     try {
+        parseIds(req.params.themeId);
+        parseIds(req.params.postId);
         const id = req.params.postId;
         const post = posts.find((post) => post.id === id);
-        post ? res.status(200).send({post}) : res.sendStatus(404)
+        post ? res.status(200).send({post}) : res.status(404).send({
+            status: 404,
+            error: "Not found"
+        });
     } catch {
-        res.status(400).send('Get failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
 app.get('/api/themes/:themeId/posts/:postId/comments/:commentId', (req, res) => {
     try {
+        parseIds(req.params.themeId);
+        parseIds(req.params.postId);
+        parseIds(req.params.commentId);
         const id = req.params.commentId;
         const comment = comments.find((comment) => comment.id === id);
-        comment ? res.status(200).send(comment) : res.sendStatus(404);
+        comment ? res.status(200).send(comment) : res.status(404).send({
+            status: 404,
+            error: "Not found"
+        });
     } catch {
-        res.status(400).send('Get failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
@@ -130,38 +175,48 @@ app.post('/api/themes', (req, res) => {
     try {
         const reqBody = req.body;
         themes.push(reqBody);
-        // res.status(200).send(`Post successful: \n ${JSON.stringify(themes)}`);
-        res.status(200).send({...themes, "status": "ok"})
+        res.status(201).send({reqBody, "status": "ok"})
     } catch {
-        res.status(400).send('Post failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
 app.post('/api/themes/:id/posts', (req, res) => {
     try {
+        parseIds(req.params.id);
         const reqBody = req.body;
         posts.push(reqBody);
-        // res.status(200).send(`Post successful: \n ${JSON.stringify(posts)}`);
-        res.status(200).send({...posts, "status": "ok"})
+        res.status(201).send({reqBody, "status": "ok"})
     } catch {
-        res.status(400).send('Post failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
 app.post('/api/themes/:themeId/posts/:postId/comments', (req, res) => {
     try {
+        parseIds(req.params.themeId);
+        parseIds(req.params.postId);
         const reqBody = req.body;
         comments.push(reqBody);
-        // res.status(200).send(`Post successful \n ${JSON.stringify(comments)}`);
-        res.status(200).send({...comments, "status": "ok"})
+        res.status(201).send({reqBody, "status": "ok"})
     } catch {
-        res.status(400).send('Post failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
 // PUT
 app.patch('/api/themes/:id', (req, res) => {
     try {
+        parseIds(req.params.id);
         const id = req.params.id;
         const data = req.body;
         const index = themes.findIndex((theme) => theme.id === parseInt(id));
@@ -172,12 +227,17 @@ app.patch('/api/themes/:id', (req, res) => {
             res.sendStatus(404);
         };
     } catch {
-        res.status(400).send('Put failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
 app.patch('/api/themes/:themeId/posts/:postId', (req, res) => {
     try {
+        parseIds(req.params.themeId);
+        parseIds(req.params.postId);
         const id = req.params.postId;
         const data = req.body;
         const index = posts.findIndex((post) => post.id === id);
@@ -188,12 +248,18 @@ app.patch('/api/themes/:themeId/posts/:postId', (req, res) => {
             res.sendStatus(404);
         }
     } catch {
-        res.status(400).send('Put failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
 app.patch('/api/themes/:themeId/posts/:postId/comments/:commentId', (req, res) => {
     try {
+        parseIds(req.params.themeId);
+        parseIds(req.params.postId);
+        parseIds(req.params.commentId);
         const id = req.params.commentId;
         const data = req.body;
         const index = comments.findIndex((comment) => comment.id === id);
@@ -201,42 +267,79 @@ app.patch('/api/themes/:themeId/posts/:postId/comments/:commentId', (req, res) =
             comments[index] = data;
             res.status(200).send(comments[index]);
         } else {
-            res.sendStatus(404);
+            res.status(404).send({
+                status: 404,
+                error: 'Not found.'
+            });
         }
     } catch {
-        res.status(400).send('Put failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
 // DELETE
 app.delete('/api/themes/:id', (req, res) => {
     try{
+        parseIds(req.params.id);
         const id = req.params.id;
         const entity = themes.find((theme) => theme.id === parseInt(id));
-        entity ? res.sendStatus(204) : res.sendStatus(404);
+        entity ? res.sendStatus(204) : res.status(404).send({
+            status: 404,
+            error: "Not found"
+        });
     } catch {
-        res.status(400).send('Delete failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     }
 });
 
 app.delete('/api/themes/:themeId/posts/:postId', (req, res) => {
     try {
+        parseIds(req.params.themeId);
+        parseIds(req.params.postId);
         const id = req.params.postId;
         const entity = posts.find((post) => post.id === id);
-        entity ? res.sendStatus(204) : res.sendStatus(404);
+        entity ? res.sendStatus(204) : res.status(404).send({
+            status: 404,
+            error: "Not found"
+        });
     } catch {
-        res.status(400).send('Delete failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
 });
 
 app.delete('/api/themes/:themeId/posts/:postId/comments/:commentId', (req, res) => {
     try {
+        parseIds(req.params.themeId);
+        parseIds(req.params.postId);
+        parseIds(req.params.commentId);
         const id = req.params.commentId;
         const entity = comments.find((comment) => comment.id === id);
-        entity ? res.sendStatus(204) : res.sendStatus(404);
+        entity ? res.sendStatus(204) : res.status(404).send({
+            status: 404,
+            error: "Not found"
+        });
     } catch {
-        res.status(400).send('Delete failed. Please try again.');
+        res.status(400).send({
+            status: 400,
+            error: "Invalid URL"
+        });
     };
+});
+
+app.use((req, res, next) => {
+    res.status(404).send({
+        status: 404,
+        error: "Not found"
+    });
 });
 
 // SET SERVER TO LISTEN TO PORT 3000
