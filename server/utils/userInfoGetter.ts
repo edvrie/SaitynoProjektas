5bcd954b778cd9dad3448d422f7ac69458bd393d
Edvinas Riepsas <edvinas.riepsas@gmail.com>
2021-11-24 23:15:25 +0300

    Added id validations

--- server/utils/userInfoGetter.ts
+++ server/utils/userInfoGetter.ts
@@ -1,16 +1,37 @@
 import mongoose from 'mongoose';
 import { connectDB, disconnectDB } from '../config/db';
 import userModel from '../schemas/user';
+import ApiError from './ApiError';
 
-export const getCurrentUser = async (email: string, password: string) => {
-    try{
-        const user = await userModel.findOne({
-            email: email,
-            password: password
-        });
-        console.log(user)
-        return user;
-    } catch (e) {
-        console.log(e);
+export const validateId = (req, res, next) => {
+    if (req.params.id){
+        const match = req.params.id.match(/^[0-9a-fA-F]{24}$/);
+        if (match === null){
+            next(ApiError.badRequest("Bad id"));
+            return;
+        }
     }
-};
+    if (req.params.themeId) {
+        const matchTheme = req.params.themeId.match(/^[0-9a-fA-F]{24}$/);
+        if (matchTheme === null){
+            next(ApiError.badRequest("Bad theme id"));
+            return;
+        }
+    }
+    if (req.params.postId) {
+        const matchPost = req.params.postId.match(/^[0-9a-fA-F]{24}$/);
+        if (matchPost === null){
+            next(ApiError.badRequest("Bad post id"));
+            return;
+        }
+    }
+    if (req.params.commentId) {
+        const matchComment = req.params.commentId.match(/^[0-9a-fA-F]{24}$/);
+        if (matchComment === null){
+            next(ApiError.badRequest("Bad comment id"));
+            return;
+        }
+    }
+    next();
+    return;
+}
