d4a7af9f90584b516edd26972c567fd20b6f9852
Edvinas Riepsas <edvinas.riepsas@gmail.com>
2021-11-30 16:07:45 +0300

    Added cors

--- server/src/api.ts
+++ server/src/api.ts
@@ -12,6 +12,7 @@ import { apiErrorHelper } from '../utils/errorHelper';
 import ApiError from '../utils/ApiError';
 import { permissionCheckGeneric, permissionCheckThemes, permissionCheckPosts, permissionCheckComments, permissionCheckAdmin, USER_ROLES } from '../utils/roleHelper';
 import { validateId } from '../utils/userInfoGetter';
+import cors from 'cors';
 
 //Config
 dotenv.config({ path: './config/config.env' });
@@ -21,15 +22,9 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 const port = process.env.PORT || 3000;
 
-interface USER {
-    username: string;
-    userId: string;
-    userRole: string;
-    auth: string;
-}
-
 // JSON parse
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
